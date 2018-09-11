32f6d6d1ca011d6542c86c0b00a18c5385640ffd
Alex Lunyov <isuntc@gmail.com>
2018-09-12 00:48:13 +0800

    Playing with new parsec-lib

--- src/components/app.js
+++ src/components/app.js
@@ -5,28 +5,57 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-import React, { Fragment } from 'react';
-// import PropTypes from 'prop-types';
-import { Route, withRouter } from 'react-router';
+/* eslint-disable react/jsx-no-bind, class-methods-use-this */
 
-import Home from '../routes/home';
+import React from 'react';
+import Web3 from 'web3';
+import { Tx, Input, Output, Outpoint } from 'parsec-lib';
 
 import '../style.css';
 
+const web3 = new Web3(window.web3.currentProvider);
+console.log(web3);
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  handleSign() {
+    const tx = Tx.transfer(
+      [
+        new Input(
+          new Outpoint(
+            '0x0000000000000000000000000000000000000000000000000000000000000000',
+            0
+          )
+        ),
+        new Input(
+          new Outpoint(
+            '0x0000000000000000000000000000000000000000000000000000000000000000',
+            1
+          )
+        ),
+      ],
+      [new Output(100, '0x8ab21c65041778dfc7ec7995f9cdef3d5221a5ad', 0)]
+    );
+
+    tx.signWeb3(web3).then(signedTx => {
+      console.log(signedTx);
+    });
+    // const address = '0x8ab21c65041778dfc7ec7995f9cdef3d5221a5ad';
+    // web3.eth.sign(tx.sigHash(), address).then(sig => {
+    //   const { r, s, v } = ethUtil.fromRpcSig(sig);
+    //   tx.inputs.forEach(input => {
+    //     input.setSig(r, s, v, address);
+    //   });
+    //   console.log();
+    // });
   }
+
   render() {
     return (
-      <Fragment>
-        <Route path="/" exact component={Home} />
-      </Fragment>
+      <div>
+        <button onClick={this.handleSign.bind(this)}>Sign</button>
+      </div>
     );
   }
 }
 
-App.propTypes = {};
-
-export default withRouter(App);
+export default App;
